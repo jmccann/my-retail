cc816e6f25e823bcf203ac0faffab4dc32712a9b
Jacob McCann <jacob.mccann2@target.com>
2018-02-20 22:30:45 -0600

    Have review highlights show side by side

--- src/CustomerReviews.js
+++ src/CustomerReviews.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 
 import ReviewHighlight from './ReviewHighlight.js';
 
+import './CustomerReviews.css';
+
 class CustomerReviews extends Component {
   render() {
     return (
       <div>
         <div>Overall: {this.props.reviews.consolidatedOverallRating}</div>
         <div>View all {this.props.reviews.totalReviews} reviews</div>
-        <ReviewHighlight type='Pro' review={this.props.reviews.Pro[0]} />
-        <ReviewHighlight type='Con' review={this.props.reviews.Con[0]} />
+        <div className="review-table">
+          <div className="review-table-row">
+            <div className="review-table-cell">
+              <ReviewHighlight type='Pro' review={this.props.reviews.Pro[0]} />
+            </div>
+            <div className="review-table-cell">
+              <ReviewHighlight type='Con' review={this.props.reviews.Con[0]} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
