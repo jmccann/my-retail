39b0439b2b809f63a12d56a3962497ddd871d9de
Jacob McCann <jacob.mccann2@target.com>
2018-02-21 19:41:41 -0600

    Format Cart

--- src/Cart.js
+++ src/Cart.js
@@ -3,14 +3,25 @@ import Button from 'muicss/lib/react/button';
 
 import Quantity from './Quantity.js';
 
+import './Cart.css';
+
 class Cart extends Component {
   render() {
     return (
       <div>
         <Quantity />
-        <Button className="pickup-in-store" color="danger" disabled={this.props.purchasingChannelCode === "1"}>Pickup In Store</Button>
-        <Button className="add-to-cart" color="danger" disabled={this.props.purchasingChannelCode === "2"}>Add To Cart</Button><br />
-        find in a store
+
+        <div className="cart-table">
+          <div className="cart-row">
+            <div className="cart-cell"><Button className="pickup-in-store" color="danger" disabled={this.props.purchasingChannelCode === "1"}>Pickup In Store</Button></div>
+            <div className="cart-cell"><Button className="add-to-cart" color="danger" disabled={this.props.purchasingChannelCode === "2"}>Add To Cart</Button></div>
+          </div>
+          <div className="cart-row">
+            <div className="cart-cell"><b>find in a store</b></div>
+            <div className="cart-cell"></div>
+          </div>
+        </div>
+
       </div>
     );
   }
