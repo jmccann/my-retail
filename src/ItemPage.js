90c2c7f0415544e383395f6b54fb7c61c39c4d80
Jacob McCann <jacob.mccann2@target.com>
2018-02-22 00:53:56 -0600

    Make site "responsive"

--- src/ItemPage.js
+++ src/ItemPage.js
@@ -15,30 +15,28 @@ class ItemPage extends Component {
     return (
       <div>
 
-        <div className="page-table">
-          <div className="page-table-row">
-
-            <div className="page-table-cell">
-              <h2>{this.props.item.title}</h2>
-              <Images items={this.props.item.Images} />
-              <CustomerReviews reviews={this.props.item.CustomerReview[0]} />
-            </div>
-
-            <div className="page-table-cell">
-              <Offer offer={this.props.item.Offers[0].OfferPrice[0]} />
-              <hr />
-              <Promotions promos={this.props.item.Promotions} />
-              <hr />
-              <Cart purchasingChannelCode={this.props.item.purchasingChannelCode} />
-              <ReturnPolicy returnPolicy={this.props.item.ReturnPolicy[0]} />
-              <button className="grey-button">Add To Registry</button>
-              <button className="grey-button">Add To List</button>
-              <button className="grey-button">Share</button>
-              <ItemDescription features={this.props.item.ItemDescription[0].features} />
-            </div>
-
+        <div className="container">
+          <div className="carousel">
+            <h2>{this.props.item.title}</h2>
+            <Images items={this.props.item.Images} />
           </div>
+          <div><Offer offer={this.props.item.Offers[0].OfferPrice[0]} /></div>
+          <div>
+            <hr />
+            <Promotions promos={this.props.item.Promotions} />
+            <hr />
+          </div>
+          <div><Cart purchasingChannelCode={this.props.item.purchasingChannelCode} /></div>
+          <div><ReturnPolicy returnPolicy={this.props.item.ReturnPolicy[0]} /></div>
+          <div>
+            <button className="grey-button">Add To Registry</button>
+            <button className="grey-button">Add To List</button>
+            <button className="grey-button">Share</button>
+          </div>
+          <div><ItemDescription features={this.props.item.ItemDescription[0].features} /></div>
+          <div><CustomerReviews reviews={this.props.item.CustomerReview[0]} /></div>
         </div>
+
       </div>
     );
   }
