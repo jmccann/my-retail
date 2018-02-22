45478cd7fa4f471737e6693ea174c8caf3be229e
Jacob McCann <jacob.mccann2@target.com>
2018-02-22 14:17:17 -0600

    Update how to find input fields

--- src/Quantity.test.js
+++ src/Quantity.test.js
@@ -13,26 +13,26 @@ it('renders without crashing', () => {
 
 test('increase quantity', () => {
   const wrapper = mount(<Quantity />);
-  expect(wrapper.find('.quantity').props().defaultValue).toBe(1);
+  expect(wrapper.find('input').props().defaultValue).toBe(1);
   wrapper.find('Button.increase').simulate('click');
-  expect(wrapper.find('.quantity').props().defaultValue).toBe(2);
+  expect(wrapper.find('input').props().defaultValue).toBe(2);
   wrapper.find('Button.increase').simulate('click');
-  expect(wrapper.find('.quantity').props().defaultValue).toBe(3);
+  expect(wrapper.find('input').props().defaultValue).toBe(3);
 });
 
 test('decrease quantity', () => {
   const wrapper = mount(<Quantity />);
   wrapper.find('Button.increase').simulate('click');
-  expect(wrapper.find('.quantity').props().defaultValue).toBe(2);
+  expect(wrapper.find('input').props().defaultValue).toBe(2);
   wrapper.find('Button.decrease').simulate('click');
-  expect(wrapper.find('.quantity').props().defaultValue).toBe(1);
+  expect(wrapper.find('input').props().defaultValue).toBe(1);
 });
 
 test('quantity can not be less then 1', () => {
   const wrapper = mount(<Quantity />);
-  expect(wrapper.find('.quantity').props().defaultValue).toBe(1);
+  expect(wrapper.find('input').props().defaultValue).toBe(1);
   wrapper.find('Button.decrease').simulate('click');
-  expect(wrapper.find('.quantity').props().defaultValue).toBe(1);
+  expect(wrapper.find('input').props().defaultValue).toBe(1);
 });
 
 it('disables decrease button by default', () => {
@@ -43,14 +43,14 @@ it('disables decrease button by default', () => {
 it('enables decrease button when greater then 1', () => {
   const wrapper = mount(<Quantity />);
   wrapper.find('Button.increase').simulate('click');
-  expect(wrapper.find('.quantity').props().defaultValue).toBe(2);
+  expect(wrapper.find('input').props().defaultValue).toBe(2);
   expect(wrapper.find('Button.decrease').props().disabled).toBe(false);
 });
 
 it('disables decrease button when quantity reaches 1', () => {
   const wrapper = mount(<Quantity />);
   wrapper.find('Button.increase').simulate('click');
-  expect(wrapper.find('.quantity').props().defaultValue).toBe(2);
+  expect(wrapper.find('input').props().defaultValue).toBe(2);
   expect(wrapper.find('Button.decrease').props().disabled).toBe(false);
   wrapper.find('Button.decrease').simulate('click');
   expect(wrapper.find('Button.decrease').props().disabled).toBe(true);
