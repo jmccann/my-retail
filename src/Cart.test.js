d0ea85f7ab5c6999b11d3235c6fe2ff96137a0ff
Jacob McCann <jacob.mccann2@target.com>
2018-02-22 23:35:06 -0600

    Fix test names

--- src/Cart.test.js
+++ src/Cart.test.js
@@ -19,13 +19,13 @@ it('has both purchase buttons enabled on purchasingChannelCode 0', () => {
   expect(wrapper.find('.add-to-cart').props().disabled).toBe(false);
 });
 
-it('has both purchase buttons enabled on purchasingChannelCode 1', () => {
+it('has only "pickup in store" button enabled on purchasingChannelCode 1', () => {
   const wrapper = shallow(<Cart purchasingChannelCode="1" />);
   expect(wrapper.find('.pickup-in-store').props().disabled).toBe(true);
   expect(wrapper.find('.add-to-cart').props().disabled).toBe(false);
 });
 
-it('has both purchase buttons enabled on purchasingChannelCode 2', () => {
+it('has only "add to cart" button enabled on purchasingChannelCode 2', () => {
   const wrapper = shallow(<Cart purchasingChannelCode="2" />);
   expect(wrapper.find('.pickup-in-store').props().disabled).toBe(false);
   expect(wrapper.find('.add-to-cart').props().disabled).toBe(true);
